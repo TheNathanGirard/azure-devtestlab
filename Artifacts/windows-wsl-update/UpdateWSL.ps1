0afe69c4ed2fa06142d273c9bc853ae777416dca
Nathan Girard <nathan@girardhome.com>
2026-01-22 09:10:44 -0600

    Updates

---
+++ Artifacts/windows-wsl-update/UpdateWSL.ps1
@@ -0,0 +1,9 @@
+
+Write-Output "Updating Windows Subsystem for Linux Feature"    
+# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+Invoke-Command -ScriptBlock { 
+    wsl.exe --update
+    wsl.exe --set-default-version 2
+    wsl.exe --install Ubuntu
+}
+
---
+++ Artifacts/windows-wsl/EnableWSL.ps1
@@ -0,0 +1,3 @@
+
+Write-Output "Enabling Windows Subsystem for Linux Feature"    
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
